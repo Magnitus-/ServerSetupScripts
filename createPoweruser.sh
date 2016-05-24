3311d83b603ac842c42b941b9c8cd35856dcef91
Eric Vallee <eric_vallee@webificservices.com>
2016-05-23 23:00:36 -0400

    Added poweruser creation script

---
+++ createPoweruser.sh
@@ -0,0 +1,9 @@
+if [ "$(whoami)" != "root" ]; then
+    echo "This script requires root privileges";
+    exit 1;
+fi
+useradd $1 -s /bin/bash -m;
+echo "Input password for ${1}: ";
+read password;
+echo ${1}:$password | chpasswd;
+echo "${1} ALL=(ALL:ALL) ALL" >> /etc/sudoers;
