f03cb36134c23daf1db352ecb9c40c37ae8a6dfc
Eric Vallee <eric_vallee@webificservices.com>
2018-04-07 11:47:53 -0400

    Added security group to test server to allow sshing into it. Added destroy hook to delete locally generated keys when infrastructure is torn down.

--- test-environment/aws_test_server.tf
+++ test-environment/aws_test_server.tf
@@ -6,6 +6,11 @@ resource "null_resource" "ssh_key" {
   provisioner "local-exec" {
     command = "ssh-keygen -t rsa -b 4096 -N '' -f $(pwd)/key"
   }
+
+  provisioner "local-exec" {
+    command = "rm key* || true"
+    when = "destroy"
+  }
 }
 
 data "local_file" "ssh_public_key" {
@@ -31,8 +36,28 @@ resource "aws_key_pair" "test_server" {
   public_key = "${data.local_file.ssh_public_key.content}"
 }
 
+resource "aws_security_group" "test_server" {
+  name = "test_server"
+  description = "Allow all outbound traffic. Allow ssh only for inbound"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "test_server" {
   ami           = "${data.aws_ami.debian.id}"
   instance_type = "t2.small"
   key_name = "test-server-key"
+  security_groups = ["${aws_security_group.test_server.name}"]
 }
