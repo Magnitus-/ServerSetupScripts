7d900785b0c6a26c23f63e6e856119cc4da6eb93
Eric Vallee <eric_vallee@webificservices.com>
2018-04-23 23:07:45 -0400

    Added cluster environment

--- README.md
+++ README.md
@@ -4,11 +4,13 @@ Scripts to remotely setup hosts via ansible
 
 As I'm mostly using Docker, the requirements are quite minimal.
 
-## Setup Test environment
+## Setup Test Environment
 
 You need terraform for this. Note that the test environment is on AWS, so you should not be running it indefinitely.
 
-Go in ```test-environment``` and type:
+### Single Server
+
+Go in ```test-server``` and type:
 
 ```
 terraform apply
@@ -22,15 +24,25 @@ To destroy it, you can simply type:
 terraform destroy
 ```
 
+### Cluster (3 machines)
+
+Same steps as the single server environment, but under the ```test-cluster``` directory instead
+
 ## Sshing Into Test environment
 
-Go in ```test-environment``` and look at the **inventory** file to get the test server ip.
+### Single Server
+
+Go in ```test-server``` and look at the **inventory** file to get the test server ip.
 
 From there, in the **test-environment** directory, type:
 
 ```
 ssh -i key admin@<ip of the host>
 ```
+
+### cluster
+
+Similar to the single server, but under the ```test-cluster``` directory. Also note that the **inventory** file will content a list of several machines and you can ssh into any one of them.
 
 ## Test Ansible roles
 
@@ -39,7 +51,7 @@ ssh -i key admin@<ip of the host>
 From the top-level directory, type:
 
 ```
-ansible-playbook test-playbooks/install_docker.yml --private-key=test-environment/key -u admin -i test-environment/inventory
+ansible-playbook test-playbooks/install_docker.yml --private-key=test-server/key -u admin -i test-server/inventory
 ```
 
 ### Python python-packages
@@ -49,7 +61,7 @@ The latest version of the **pip** tool and the **docker** package (version 3.2.1
 From the top-level directory, type:
 
 ```
-ansible-playbook test-playbooks/install_python_packages.yml --private-key=test-environment/key -u admin -i test-environment/inventory
+ansible-playbook test-playbooks/install_python_packages.yml --private-key=test-server/key -u admin -i test-server/inventory
 ```
 
 ### kubeadm
@@ -59,7 +71,7 @@ kubeadm, kubelet and kubectl will be installed. Note that docker is expected to 
 From the top-level directory, type:
 
 ```
-ansible-playbook test-playbooks/install_kubeadm.yml --private-key=test-environment/key -u admin -i test-environment/inventory
+ansible-playbook test-playbooks/install_kubeadm.yml --private-key=test-server/key -u admin -i test-server/inventory
 ```
 
 ### k8 master
@@ -69,5 +81,5 @@ Initialize a k8 master using flannel as the network driver. Note that both docke
 From the top-level directory, type:
 
 ```
-ansible-playbook test-playbooks/install_k8_master.yml --private-key=test-environment/key -u admin -i test-environment/inventory
+ansible-playbook test-playbooks/install_k8_master.yml --private-key=test-server/key -u admin -i test-server/inventory
 ```
---
+++ test-cluster/aws_test_cluster.tf
@@ -0,0 +1,112 @@
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_default_vpc" "default" {
+}
+
+resource "null_resource" "ssh_key" {
+  provisioner "local-exec" {
+    command = "ssh-keygen -t rsa -b 4096 -N '' -f $(pwd)/key"
+  }
+
+  provisioner "local-exec" {
+    command = "rm key* || true"
+    when = "destroy"
+  }
+}
+
+data "local_file" "ssh_public_key" {
+  filename = "${path.module}/key.pub"
+  depends_on = ["null_resource.ssh_key"]
+}
+
+#https://wiki.debian.org/Cloud/AmazonEC2Image/Stretch
+data "aws_ami" "debian" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["debian-stretch-hvm-x86_64-gp2-2018-03-11-19642"]
+  }
+
+  owners = ["379101102735"]
+}
+
+resource "aws_key_pair" "test_cluster" {
+  depends_on = ["null_resource.ssh_key"]
+  key_name   = "test-cluster-key"
+  public_key = "${data.local_file.ssh_public_key.content}"
+}
+
+resource "aws_security_group" "test_cluster_ssh" {
+  name = "test_cluster_ssh"
+  description = "Allow inbound ssh traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "test_cluster_outbound" {
+  name = "test_cluster_outbound"
+  description = "Allow all outbound traffic"
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "test_cluster_internal" {
+  name = "test_cluster_internal"
+  description = "Allow all internal traffic"
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["${aws_default_vpc.default.cidr_block}"]
+  }
+
+}
+
+resource "aws_instance" "test_pods" {
+  ami           = "${data.aws_ami.debian.id}"
+  instance_type = "t2.small"
+  key_name = "test-cluster-key"
+  security_groups = ["${aws_security_group.test_cluster_ssh.name}", "${aws_security_group.test_cluster_outbound.name}", "${aws_security_group.test_cluster_internal.name}"]
+  count = 2
+}
+
+resource "aws_instance" "test_master" {
+  ami           = "${data.aws_ami.debian.id}"
+  instance_type = "t2.small"
+  key_name = "test-cluster-key"
+  security_groups = ["${aws_security_group.test_cluster_ssh.name}", "${aws_security_group.test_cluster_outbound.name}", "${aws_security_group.test_cluster_internal.name}"]
+}
+
+resource "null_resource" "inventory" {
+  triggers {
+    test_pod_id = "${join(",", aws_instance.test_pods.*.id)}"
+    test_master_id = "${join(",", aws_instance.test_master.*.id)}"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '[test_master]\n${aws_instance.test_master.public_ip}\n\n' > inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "echo '[test_pods]\n${join("\n", aws_instance.test_pods.*.public_ip)}' >> inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "rm inventory || true"
+    when = "destroy"
+  }
+}
