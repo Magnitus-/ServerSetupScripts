a18683bc360278d27f1f8fd0299b69de0b1aacfc
Eric Vallee <eric_vallee@webificservices.com>
2018-05-23 23:28:28 -0400

    Etcd pod manifest cleanup

--- roles/etcd/files/setup_etcd_pod.sh
+++ roles/etcd/files/setup_etcd_pod.sh
@@ -1,5 +1,4 @@
 
-#etcd0=https://<etcd0-ip-address>:2380,etcd1=https://<etcd1-ip-address>:2380,etcd2=https://<etcd2-ip-address>:2380
 INITIAL_CLUSTER=""
 INDEX=$((0))
 
@@ -19,42 +18,58 @@ cat >/etc/kubernetes/manifests/etcd.yaml <<EOF
 apiVersion: v1
 kind: Pod
 metadata:
-labels:
+  labels:
     component: etcd
     tier: control-plane
-name: etcd_${MASTER_ID}
-namespace: kube-system
+  name: etcd_${MASTER_ID}
+  namespace: kube-system
 spec:
-containers:
-- command:
-    - etcd --name  - --data-dir /var/lib/etcd - --listen-client-urls https://:2379 - --advertise-client-urls https://:2379 - --listen-peer-urls https://:2380 - --initial-advertise-peer-urls https://:2380 - --cert-file=/certs/server.pem - --key-file=/certs/server-key.pem - --client-cert-auth - --trusted-ca-file=/certs/ca.pem - --peer-cert-file=/certs/peer.pem - --peer-key-file=/certs/peer-key.pem - --peer-client-cert-auth - --peer-trusted-ca-file=/certs/ca.pem - --initial-cluster ${INITIAL_CLUSTER} - --initial-cluster-token my-etcd-token - --initial-cluster-state new
+  containers:
+  - name: etcd
     image: k8s.gcr.io/etcd-amd64:3.1.10
+    command: ["etcd"]
+    args:
+    - --data-dir=/var/lib/etcd
+    - --listen-client-urls https://localhost:2379
+    - --advertise-client-urls https://localhost:2379
+    - --listen-peer-urls https://localhost:2380
+    - --initial-advertise-peer-urls https://localhost:2380
+    - --cert-file=/certs/server.pem
+    - --key-file=/certs/server-key.pem
+    - --client-cert-auth
+    - --trusted-ca-file=/certs/ca.pem
+    - --peer-cert-file=/certs/peer.pem
+    - --peer-key-file=/certs/peer-key.pem
+    - --peer-client-cert-auth
+    - --peer-trusted-ca-file=/certs/ca.pem
+    - --initial-cluster ${INITIAL_CLUSTER}
+    - --initial-cluster-token my-etcd-token
+    - --initial-cluster-state new
     livenessProbe:
-    httpGet:
+      httpGet:
         path: /health
         port: 2379
         scheme: HTTP
-    initialDelaySeconds: 15
-    timeoutSeconds: 15
-    name: etcd
+      initialDelaySeconds: 15
+      timeoutSeconds: 15
     env:
     - name: PUBLIC_IP
-    valueFrom:
+      valueFrom:
         fieldRef:
-        fieldPath: status.hostIP
+          fieldPath: status.hostIP
     - name: PRIVATE_IP
-    valueFrom:
+      valueFrom:
         fieldRef:
-        fieldPath: status.podIP
+          fieldPath: status.podIP
     - name: PEER_NAME
-    valueFrom:
+      valueFrom:
         fieldRef:
-        fieldPath: metadata.name
+          fieldPath: metadata.name
     volumeMounts:
     - mountPath: /var/lib/etcd
-    name: etcd
+      name: etcd
     - mountPath: /certs
-    name: certs
+      name: certs
 hostNetwork: true
 volumes:
 - hostPath:
