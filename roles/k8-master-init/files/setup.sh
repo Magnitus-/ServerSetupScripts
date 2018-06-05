6a1ef4b69d14dac32644ee1d1d77795687e9cc7a
Eric Vallee <eric_vallee@webificservices.com>
2018-06-05 09:53:30 -0400

    Moved swapoff to the installation of kubeadm since it is the common denominator for k8 nodes

--- roles/k8-master-init/files/setup.sh
+++ roles/k8-master-init/files/setup.sh
@@ -30,7 +30,6 @@ apiServerExtraArgs:
   apiserver-count: "${INDEX}"
 EOF
 
-    swapoff -a;
     kubeadm init --config=/opt/config.yaml;
     rm /opt/config.yaml;
 
--- roles/kubeadm/files/install_kubeadm.sh
+++ roles/kubeadm/files/install_kubeadm.sh
@@ -7,6 +7,7 @@ if [ ! -x "$(command -v kubeadm)" ]; then
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-    apt-get update
-    apt-get install -y kubelet kubeadm kubectl
+    apt-get update;
+    apt-get install -y kubelet kubeadm kubectl;
+    swapoff -a;
 fi
