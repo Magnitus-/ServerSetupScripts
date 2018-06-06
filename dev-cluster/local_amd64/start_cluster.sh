c5253813cc367bd107ed19ba6a8a90dcdc34bfd9
Eric Vallee <eric_vallee@webificservices.com>
2018-06-05 22:23:26 -0400

    Fixes to some race conditions

--- dev-cluster/local_amd64/start_cluster.sh
+++ dev-cluster/local_amd64/start_cluster.sh
@@ -1,3 +1,10 @@
 for VM in $(echo "master0 master1 master2 worker0 worker1 worker2 lbl0"); do
     virsh start $VM;
 done
+
+for VM in $(echo "master0 master1 master2 worker0 worker1 worker2 lbl0"); do
+    VM_UP=$(virsh list | grep $VM)
+    while [ -z "$VM_UP" ]; do
+        VM_UP=$(virsh list | grep $VM)
+    done
+done
