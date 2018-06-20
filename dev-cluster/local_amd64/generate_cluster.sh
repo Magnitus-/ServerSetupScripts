8b11e2809aa1cdd4142380bc65b4ee71b02b45b0
Eric Vallee <eric_vallee@webificservices.com>
2018-06-20 01:37:17 -0400

    Modifed local amd64 environment to accomodate an emulated arm64 environment

--- dev-cluster/local_amd64/generate_cluster.sh
+++ dev-cluster/local_amd64/generate_cluster.sh
@@ -3,13 +3,14 @@
 WORKER_RAM=${WORKER_RAM:-4G}
 MASTER_RAM=${MASTER_RAM:-2G}
 LBL_RAM=${LBL_RAM:-1G}
+VM_TEMPLATE=${VM_TEMPLATE:-"k8_vm_template"}
 
 MASTER0=$(virsh list --name --all | grep master0)
 
 if [ -z "$MASTER0" ]; then
 
     for VM in $(echo "master0 master1 master2 worker0 worker1 worker2 lbl0"); do
-        virt-clone --original=k8_vm_template --name=${VM} --file=$(pwd)/disks/${VM}.img;
+        virt-clone --original=${VM_TEMPLATE} --name=${VM} --file=$(pwd)/disks/${VM}.img;
 
         if [[ "$VM" =~ "master" ]]; then
             RAM=$MASTER_RAM
--- dev-cluster/local_amd64/generate_image.sh
+++ dev-cluster/local_amd64/generate_image.sh
@@ -2,23 +2,38 @@
 
 DISK_SIZE=${DISK_SIZE:-4}
 VIRTUAL_CPUS=${VIRTUAL_CPUS:-4}
+VM_TEMPLATE=${VM_TEMPLATE:-"k8_vm_template"}
 
-K8_VM_TEMPLATE=$(virsh list --name --all | grep k8_vm_template)
+K8_VM_TEMPLATE=$(virsh list --name --all | grep $VM_TEMPLATE)
+
+if [ "$VM_TEMPLATE" = "k8_vm_template"  ]; then
+    VM_IMAGE_LOCATION=http://ftp.us.debian.org/debian/dists/stable/main/installer-amd64/
+    VM_CPU=host-passthrough,cache.mode=passthrough
+    VM_VIRT_TYPE=kvm
+    VM_ARCHITECTURE=x86_64
+else
+    VM_IMAGE_LOCATION=http://ftp.us.debian.org/debian/dists/stable/main/installer-arm64/
+    VM_CPU=cortex-a57
+    VM_VIRT_TYPE=qemu
+    VM_ARCHITECTURE=aarch64
+fi
+
 
 if [ -z "$K8_VM_TEMPLATE" ]; then
     mkdir -p $(pwd)/disks;
 
     virt-install \
-    --name=k8_vm_template \
+    --name=$VM_TEMPLATE \
     --ram=1024 \
     --vcpus=$VIRTUAL_CPUS \
-    --cpu host-passthrough,cache.mode=passthrough \
+    --cpu $VM_CPU \
     --disk size=$DISK_SIZE,path=$(pwd)/disks/k8_template.img,bus=virtio,cache=none \
     --initrd-inject=preseed.cfg \
-    --location http://ftp.us.debian.org/debian/dists/stable/main/installer-amd64/ \
+    --location $VM_IMAGE_LOCATION \
     --os-type linux \
     --os-variant debian9 \
-    --virt-type=kvm \
+    --arch $VM_ARCHITECTURE \
+    --virt-type=$VM_VIRT_TYPE \
     --controller usb,model=none \
     --graphics none \
     --noautoconsole \
@@ -26,9 +41,9 @@ if [ -z "$K8_VM_TEMPLATE" ]; then
     --noreboot \
     --extra-args="auto=true console=tty0 console=ttyS0,115200n8 serial"
 
-    K8_VM_TEMPLATE_RUNNING=$(virsh list --name | grep k8_vm_template);
+    K8_VM_TEMPLATE_RUNNING=$(virsh list --name | grep $VM_TEMPLATE);
     while [ ! -z "$K8_VM_TEMPLATE_RUNNING" ]; do
         sleep 2;
-        K8_VM_TEMPLATE_RUNNING=$(virsh list --name | grep k8_vm_template);
+        K8_VM_TEMPLATE_RUNNING=$(virsh list --name | grep $VM_TEMPLATE);
     done
 fi
