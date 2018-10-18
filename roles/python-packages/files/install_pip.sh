f05539cc663aa82c07dc53509221fd93403b68f4
Eric Vallee <eric_vallee@webificservices.com>
2018-10-18 02:20:40 -0400

    Small improvements to Python dependencies role

--- roles/python-packages/files/install_pip.sh
+++ roles/python-packages/files/install_pip.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+if [ ! -z "$(which python)" ]
+    PYTHON_COMMAND=python
+else
+    PYTHON_COMMAND=python3
+fi
+
 if [ ! -x "$(command -v pip)" ]; then
-    curl -fsSL https://bootstrap.pypa.io/get-pip.py | python
+    curl -fsSL https://bootstrap.pypa.io/get-pip.py | $PYTHON_COMMAND
 fi
